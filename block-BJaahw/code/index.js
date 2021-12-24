b59d22acfce9dad3debd127f65ae81a8ec40019c
Vivek Kumar <vivek32032@gmail.com>
2021-12-24 16:09:21 +0530

    add solutions

--- block-BJaahw/code/index.js
+++ block-BJaahw/code/index.js
@@ -1,19 +1,73 @@
 // 1. Create a function `createUser` that accepts `name` and `age` and returns a new object with those properties.
 
+function createUser(name,age){
+    let user = {};
+    user.name= name;
+    user.age= age;
+    return user;
+}
+
 // 2. Add a method named `sayHello` inside the object created above. When called it should alert `Welcome {user}`. Replace `{user}` with the name of the user.
+function createUser(name,age){
+    let user = {};
+    user.name= name;
+    user.age= age;
+    user.sayHello = function(){
+        alert(`Welcome ${this.name}`)
+    } 
+       return user;
 
+}
 // 3. Use the data (name, age) of two different person to create the object using `createUser`. Store the returned value in `personOne` and `personTwo`.
-
+// let personOne = createUser("vivek",24);
+// let personTwo = createUser("Amit",25);
 // 4. Change the code inside `createUser` in such a way that the methods `sayHello` doesn't have to be in all object. Use the prototypal nature. (Hint Object.create())
+function createUser(name,age){
+    let user = Object.create(userMethods);
+    user.name= name;
+    user.age= age;
+  
+    return user;
 
+}
+userMethods={
+    sayHello(){
+        alert(`Welcome ${this.name}`)
+    } 
+}
 // 5. Convert the `createUser` function into Pseudoclassical pattern of object creation. Use `F.prototype` to store the methods. `F.prototype` means storing the methods in prototype property of the function.
-
+function CreateUser(name,age){
+   this.name= name;
+   this.age= age;
+}
+CreateUser.prototype={
+    sayHello(){
+        alert(`Welcome ${this.name}`)
+    } 
+}
 // 6. Use `new` to create two new objects with the data of two different person and re-assign the value of `personOne` and `personTwo`.
+
+// let personOne = new CreateUser("vikas",26);
+// let personTwo = new CreateUser("sumit",25);
 
 // 7. Try calling `personOne.sayHello()` and `personTwo.sayHello()`. Check if you get the required output.
 
+// personOne.sayHello();
+// personTwo.sayHello();
+
 // 8. Convert the `createUser` function into `User` class.
-
+class User{
+    constructor(name,age){
+        this.name= name;
+        this.age= age;
+    }
+    sayHello(){
+        alert(`Welcome ${this.name}`)
+    } 
+}
 // 9. Check by creating two instance of the class using data of two different persons and re-assign the value of `personOne` and `personTwo`
-
+let personOne = new User("vikash",26);
+let personTwo = new User("sumit",25);
 // 10. Try calling `personOne.sayHello()` and `personTwo.sayHello()`. Check if you get the required output.
+personOne.sayHello();
+personTwo.sayHello()
